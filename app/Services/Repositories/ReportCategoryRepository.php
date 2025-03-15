0be6bc028470f34a856da4ea2ce34a72f35718ce
belajar-vue-2 <aryaadi229@gmail.com>
2025-03-15 15:58:48 +0700

    feat : deleteStorage on ReportCategoryRepository

--- app/Services/Repositories/ReportCategoryRepository.php
+++ app/Services/Repositories/ReportCategoryRepository.php
@@ -3,6 +3,7 @@
 namespace App\Services\Repositories;
 
 use App\Models\ReportCategory;
+use Illuminate\Support\Facades\Storage;
 use App\Services\Interfaces\ReportCategoryRepositoryInterface;
 
 class ReportCategoryRepository implements ReportCategoryRepositoryInterface
@@ -19,18 +20,30 @@ class ReportCategoryRepository implements ReportCategoryRepositoryInterface
     public function createReportCategory(array $data)
     {
         $data['image'] = $data['image']->store('assets/report-category', 'public');
+
         return ReportCategory::create($data);
     }
 
     public function updateReportCategory(array $data, int $id)
     {
         $reportCategory = $this->getReportCategoryById($id);
+
         if (isset($data['image'])) {
-            $data['image'] = $data['image']->store('assets/report-category/image', 'public');
+            //ensure the image is not null before updating
+            if ($reportCategory->image) {
+                //check if the image is available in storage
+                if (Storage::disk('public')->exists($reportCategory->image)) {
+                    //delete the image that is in storage
+                    Storage::disk('public')->delete($reportCategory->image);
+                }
+            }
+            $data['image'] = $data['image']->store('assets/report-category', 'public');
         }
+
         if (!isset($data['name'])) {
             $data['name'] = $reportCategory->name;
         }
+
         return $reportCategory->update($data);
     }
 
@@ -40,7 +53,16 @@ class ReportCategoryRepository implements ReportCategoryRepositoryInterface
         if ($reportCategory->reports()->count() >= 1) {
             return false;
         } else {
+            //ensure the image is not null before updating
+            if ($reportCategory->image) {
+                //check if the image is available in storage
+                if (Storage::disk('public')->exists($reportCategory->image)) {
+                    //delete the image that is in storage
+                    Storage::disk('public')->delete($reportCategory->image);
+                }
+            }
             $reportCategory->delete();
+
             return true;
         }
     }
